ba18574492559f296a91bc4a2bda453f4f58d4cc
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-24 09:55:56 +0530

    add navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-sm  navbar-dark" id="navbar">
+    <a class="navbar-brand" href="#">
+        <img src="C:\Users\Dell\Desktop\IP project\New folder\logo.jpg" alt="Avatar Logo" style="width:40px;" class="rounded-pill">
+    </a>
+    </div>
+    <div class="container-fluid">
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">home</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">services</a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="C:\Users\Dell\Desktop\class\IP project\services\mechanical issues.html">Mechanical Issues</a></li>
+                    <li><a class="dropdown-item" href="C:\Users\Dell\Desktop\class\IP project\services\electrical issues.html">Electrical Issues</a></li>
+                    <li><a class="dropdown-item" href="C:\Users\Dell\Desktop\class\IP project\services\tire issues.html">Tire-Related Issues</a></li>
+                    <li><a class="dropdown-item" href="C:\Users\Dell\Desktop\class\IP project\services\other issues.html">Other Issues</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">advices</a>
+                <ul class="dropdown-menu">
+                    <li><a href="C:\Users\Dell\Desktop\class\IP project\advice\howtodrive.html" class="dropdown-item" href="#">How To Protect Your Vehicle?</a></li>
+                    <li><a href="C:\Users\Dell\Desktop\class\IP project\advice\fatigue.html" class="dropdown-item" href="#">Driver Fatigue</a></li>
+                    <li><a href="C:\Users\Dell\Desktop\class\IP project\advice\childsafety.html" class="dropdown-item" href="#">Child Safety</a></li>
+                    <li><a href="C:\Users\Dell\Desktop\class\IP project\advice\pretrip.html" class="dropdown-item" href="#">Pre-Trip Checkup</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Aboutus</a>
+            </li>
+
+        </ul>
+        <div>
+            <form action="login page/login.html" method="get">
+                <button class="rounded-pill">Login</button>
+            </form>
+
+        </div>
+    </div>
+    </div>
+</nav>
