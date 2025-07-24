afa788adee8a987157ab6a3944035d86914524ec
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-24 10:10:27 +0530

    admin login page route added

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+Route::get('/admin-login', function () {
+    return view('pages.admin.admin_login');
+});
