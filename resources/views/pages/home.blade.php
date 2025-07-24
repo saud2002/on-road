4543cd429b9a0893b8b88d5eecfef8243463974d
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-24 10:05:37 +0530

    create home page and style

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+@section('content')
+@include('components.navbar')
+<!---home--->
+<section id="home">
+    <h1>BREAKDOWN SERVICES</h1>
+</section>
+
+<section id="Product">
+    <div class="container m-5">
+        <h1 class="text-center my-5">BREAKDOWN ROAD SERVICES</h1>
+    </div>
+</section>
+@endsection
